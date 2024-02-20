0fb766f7f09baac72d03fdba0044db6782faea08
Lucas Rausch <leaf5ter@tuta.io>
2024-02-19 22:57:11 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>iuse.</h1>
+      <p>Hello! I'm iusebako. Unprofessional internet connoisseur. Professional ADHD and Autism haver.<br/>When something breaks on my computer, or my friends want me to look into something,<br/>or even research for a school project, I'll do the research.<br/>I play all sorts of games on Steam and Roblox.</p>
+      <h3>more coming soon...</h3>
+    </>
+  )
+}
+
+export default App
