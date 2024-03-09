ec01ae03ad40325919567ee1fcc6d3f7f38e94dc
Lucas Rausch <leaf5ter@tuta.io>
2024-03-08 21:29:58 -0600

    fixalotidfk

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,16 @@ function App() {
   return (
     <>
       <h1>iuse.</h1>
-      <p>Hello! I'm iusebako. Unprofessional internet connoisseur. Professional ADHD and Autism haver.<br/>When something breaks on my computer, or my friends want me to look into something,<br/>or even research for a school project, I'll do the research.<br/>I play all sorts of games on Steam and Roblox.</p>
+      <p>Hello! I'm iusebako. Unprofessional internet connoisseur. Professional ADHD and Autism haver.<br/>Avid Self-Hoster and Programmer.<br/>When something breaks on my computer, or my friends want me to look into something,<br/>or even research for a school project, I'll do the research.<br/>I play all sorts of games on Steam and Roblox.</p>
+      <br/>
+      <h4>Apps I make:</h4>
+      <p>ScratchFixEmbed - <a href="https://github.com/real-kwellercat/ScratchFixEmbed/">link here</a><br/>Works like this: You send a link on discord or twitter with this format: https://scratchfixembed-real-kwellercat.koyeb.app/u/iusebako<br/> ofc remove iusebako and replace it with whatever username.</p>
+      <hr/>
+      <h2>DISCORD ACCOUNTS THAT iusebako OWNS</h2>
+      <img src="https://lanyard.cnrad.dev/api/1203527258002821171"/>‎ ‎ ‎ 
+      <img src="https://lanyard.cnrad.dev/api/950189411759456266"/>
+      <hr/>
+      <p>if you happen to be coming from my NotITG lobotomy videos, feel free to check out the level downloads <a href="https://www.mediafire.com/folder/8qjqi19ugbnyz/NotITG_Lobotomies">here.</a></p>
       <h3>more coming soon...</h3>
     </>
   )
